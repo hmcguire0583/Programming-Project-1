724c6e2941b5ae103eb06bc30dcb17d56cffe987
hmcguire0583 <harryjunior2026@gmail.com>
2025-10-23 00:52:47 -0400

    Did the first part (creating the first pipe fd1 along with appropriate error handling for first child/pipe error).

--- SortUniqWc/sortuniqwc.c
+++ SortUniqWc/sortuniqwc.c
@@ -1,13 +1,39 @@
 /*   
 insert #include directives for all needed header files here  */ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int main(int argc, char *arv[]) { 
 //create first pipe fd1 
 // fork first child 
+
+int fd1[2]; // create the first pipe fd1, has to be initalized with 2 ends
+
+if (pipe(fd1) < 0) { // initializing the pipe itself
+// pipe error
+fprintf(stderr, "pipe error!\n"); 
+exit(1); 
+}
+       
 pid = fork();  // create first child for sort 
+
 if (pid < 0) { 
 // fork error 
+fprintf(stderr, "forking error!\n");
+exit(1); // program terminates unsuccesfully
 } 
+
+
+// NEXT PART I HAVEN'T STARTED:
+
+
+
+
 if (pid == 0) { // first child process, run sort 
+
+// sort ()
+
 // tie write end of pipe fd1 to standard output (file descriptor 1) 
 // close read end of pipe fd1 
 // start the sort command using execlp 
