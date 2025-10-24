d5e95dfa791e685ad5962dfac26221324e4ac12c
hmcguire0583 <harryjunior2026@gmail.com>
2025-10-23 23:30:29 -0400

    added executing uniq for second child process

--- SortUniqWc/sortuniqwc.c
+++ SortUniqWc/sortuniqwc.c
@@ -27,7 +27,6 @@ fprintf(stderr, "forking error!\n");
 return 1; // program terminates unsuccesfully
 } 
 
-
 if (pid == 0) { // first child process, run sort 
 printf("The child process running sort is %d\n", getpid());     
 // tie write end of pipe fd1 to standard output (file descriptor 1) 
@@ -41,29 +40,39 @@ printf("Should not be here after execlp to sort\n"); // error message for exelcp
 exit(1); // exits after execlp failure
 } 
 
-// HARRY GOT TO HERE - FINISHED SORT
-
 //create second pipe fd2 
 int fd2[2]; // create the second pipe fd2, has to be initalized with 2 ends
 if (pipe(fd2) < 0) { // initializing the pipe itself
 // pipe error
-fprintf(stderr, "pipe error!\n"); 
-return 1; 
+fprintf(stderr, "pipe error!\n"); // pipe error message
+return 1; // return
 }
 
 // fork second child 
 pid = fork(); // create second child for uniq 
 if (pid < 0) { 
 // fork error 
+fprintf(stderr, "forking error!\n");
+return 1; // program terminates unsuccesfully
 } 
 if (pid == 0) { // second child process, run uniq 
+printf("The child process running uniq is %d\n", getpid());     
 // tie read end of fd1 to standard input (file descriptor 0) 
+dup2(fd1[0], 0);
 // tie write end of fd2 to standard output (file descriptor 1) 
+dup2(fd2[1], 1);
 // close write end of pipe fd1 
+close(fd1[1]);
 // close read end of pipe fd2 
+close(fd2[0]);
 // start the uniq command using execlp 
+execlp("/usr/bin/uniq", "uniq", NULL);
 // should not get here 
+printf("Should not be here after execlp to uniq\n"); // error message for exelcp failure
+exit(1); // exits after execlp failure
 } 
+
+
 // fork third child 
 pid = fork(); // create third child for wc -l 
 if (pid < 0) { 
