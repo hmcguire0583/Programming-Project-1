24dc802366a82d6093280f58b1d9aacf6c05102c
hmcguire0583 <harryjunior2026@gmail.com>
2025-10-23 23:15:16 -0400

    finished sort function for first child process

--- SortUniqWc/sortuniqwc.c
+++ SortUniqWc/sortuniqwc.c
@@ -2,21 +2,20 @@
 insert #include directives for all needed header files here  */ 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/wait.h> 
 
 
 int main(int argc, char *argv[]) { 
 //create first pipe fd1 
 // fork first child 
-
 int fd1[2]; // create the first pipe fd1, has to be initalized with 2 ends
 
 if (pipe(fd1) < 0) { // initializing the pipe itself
 // pipe error
 fprintf(stderr, "pipe error!\n"); 
-exit(1); 
+return 1; 
 }
 
 pid_t pid;    // pid initialized   
@@ -25,24 +24,33 @@ pid = fork(); // create first child for sort
 if (pid < 0) { 
 // fork error 
 fprintf(stderr, "forking error!\n");
-exit(1); // program terminates unsuccesfully
+return 1; // program terminates unsuccesfully
 } 
-
-
-// Harry - I GOT UP TO THIS POINT - NEXT PART I HAVEN'T STARTED - WE CAN WORK ON IT
-
 
 
 if (pid == 0) { // first child process, run sort 
-
-// sort ()
-
+printf("The child process running sort is %d\n", getpid());     
 // tie write end of pipe fd1 to standard output (file descriptor 1) 
+dup2(fd1[1], 1);
 // close read end of pipe fd1 
+close(fd1[0]); // 0 is the read end of the pipe
 // start the sort command using execlp 
+execlp("/usr/bin/sort", "sort",  NULL);
 // should not get here 
+printf("Should not be here after execlp to sort\n"); // error message for exelcp failure
+exit(1); // exits after execlp failure
 } 
+
+// HARRY GOT TO HERE - FINISHED SORT
+
 //create second pipe fd2 
+int fd2[2]; // create the second pipe fd2, has to be initalized with 2 ends
+if (pipe(fd2) < 0) { // initializing the pipe itself
+// pipe error
+fprintf(stderr, "pipe error!\n"); 
+return 1; 
+}
+
 // fork second child 
 pid = fork(); // create second child for uniq 
 if (pid < 0) { 
